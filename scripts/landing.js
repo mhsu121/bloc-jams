7171a3f4611bfe7474f65159e167fa7cfedd7f90
Michelle <mhsu0121@gmail.com>
2018-02-01 22:45:53 -0800

    assignment-23-events complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,12 +3,7 @@ var pointsArray = document.getElementsByClassName('point');
 var animatePoints = function(points) {
 
     var revealPoint = function() {
-        for (var i = 0; i < 3; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
+        forEach(points);
     };
 
     revealPoint();
