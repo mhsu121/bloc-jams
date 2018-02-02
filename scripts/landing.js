e32b70b96ff7bc470254c56cba57c1e858534972
Michelle <mhsu0121@gmail.com>
2018-02-01 22:53:47 -0800

    assignment-23-events revision complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points) {
-
-    var revealPoint = function() {
-        forEach(points);
-    };
-
-    revealPoint();
-
+    forEach(points, revealPoint);
 };
 
 window.onload = function() {
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,8 +1,5 @@
-var forEach = function(points) {
-  for (var i = 0; i < points.length; i++) {
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+function forEach (array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    callback(array[i]);
   }
-};
+}
